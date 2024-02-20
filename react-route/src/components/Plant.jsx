f5567c6c4073a0b3da55b4bb868294ae509a7278
salima <brycesalima@gmail.com>
2024-02-20 17:58:59 +0000

    css of plant container

--- react-route/src/components/Plant.jsx
+++ react-route/src/components/Plant.jsx
@@ -1,14 +1,14 @@
-// import "./components/Plant.css";
+import "./plant-container.css";
 
 export default function Plant({ plant }) {
   return (
     <div className="plantCard">
       <img src={plant.image} alt="" className="plant-image" />
-      <h4 className="">{plant.name}</h4>
-      <div className="plant-container">
-        <p>{plant.scientificName}</p>
-        <p>{plant.spiritualUses}</p>
-        <p>{plant.spellUses}</p>
+      <h4 className="thisismyplant">{plant.name}</h4>
+      <div className="plant-detail-container">
+        <p className="thisismyplant">{plant.scientificName}</p>
+        <p className="thisismyplant">{plant.spiritualUses}</p>
+        <p className="thisismyplant">{plant.spellUses}</p>
       </div>
     </div>
   );
